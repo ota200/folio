b1c936a26e7889f9f081bbde740f0c02fe78d5be
OTA <aniscratch@gmail.com>
2023-01-21 15:07:33 -0500

    Finished

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 var theme = document.getElementsByTagName('link')[0];
 var body = document.getElementsByTagName('body')
 
+
 function Theme(){
     if (theme.getAttribute('href') == 'light.css') {
         theme.setAttribute('href', 'dark.css');
         localStorage.setItem("theme","dark.css")
-        body.style.animationName = "fadeIn"
-        body.style.animationDuration = "2s"
 
 
     } else {
         theme.setAttribute('href', 'light.css');
         localStorage.setItem("theme","light.css")
-        body.style.animationName = "fadeIn"
-        body.style.animationDuration = "2s"
-        body.style.background = "red"
 
     }
 }
@@ -25,7 +21,9 @@ function set(){
     if (b == null){
         theme.setAttribute('href', "light.css");
 
-    } else {
+    } 
+
+    else {
         console.log(localStorage.getItem("theme"))
         theme.setAttribute('href', b);
     }
