8fd9317131d9b0658fe82b250cec7534b46dee74
OTA <aniscratch@gmail.com>
2023-02-21 15:36:33 -0500

    restart

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-/*var theme = document.getElementsByTagName('link')[0];
+var theme = document.getElementsByTagName('link')[0];
 var body = document.getElementsByTagName('body')
 var page = document.getElementsByClassName('homebar')
 
+// function Theme(){
+//     if (theme.getAttribute('href') == 'light.css') {
+//         theme.setAttribute('href', 'dark.css');
+//         localStorage.setItem("theme","dark.css")
 
-function Theme(){
-    if (theme.getAttribute('href') == 'light.css') {
-        theme.setAttribute('href', 'dark.css');
-        localStorage.setItem("theme","dark.css")
 
+//     } else {
+//         theme.setAttribute('href', 'light.css');
+//         localStorage.setItem("theme","light.css")
 
-    } else {
-        theme.setAttribute('href', 'light.css');
-        localStorage.setItem("theme","light.css")
+//     }
+// }
 
-    }
-}
+// function set(){
+//     b = localStorage.getItem("theme")
+//     console.log(b)
+//     if (b == null){
+//         theme.setAttribute('href', "light.css");
 
-function set(){
-    b = localStorage.getItem("theme")
-    console.log(b)
-    if (b == null){
-        theme.setAttribute('href', "light.css");
+//     } 
 
-    } 
+//     else {
+//         console.log(localStorage.getItem("theme"))
+//         theme.setAttribute('href', b);
+//     }
 
-    else {
-        console.log(localStorage.getItem("theme"))
-        theme.setAttribute('href', b);
-    }
-
-}*/
+// }
 
 const element = document.getElementById("main").children
 
+element[0].style.display = "block"
+element[1].style.display = "none"
+element[2].style.display = "none"
 
 function home(){
     element[0].style.display = "block"
@@ -54,3 +56,27 @@ function projects(){
     element[1].style.display = "none"
     element[2].style.display = "block"
 }
+
+
+
+// const blob = document.querySelector(".blob");
+
+// document.addEventListener('mousemove', e =>{
+//     blob.style.top = e.clientY+ "px";
+//     blob.style.left = e.clientX+ "px";
+
+// })
+
+// blob.addEventListener('mousemove', e =>{
+//     blob.animate({ left: e.pageX + "px", top: e.pageY + "px"},{ duration: 3000, fill:"forwards" });
+// })
+
+// document.body.onpointermove = event =>{
+//     const {clientX,clientY} = event;
+//     blob.animate({
+//         left: clientX + "vw",
+//         top:clientY + "vh"
+//     },{ duration:3000, fill:"forwards" });
+
+
+// }
